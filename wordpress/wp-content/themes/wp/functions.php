3d27f9460acd56ba7e5b6154239c5b3f18e17a80
bbbtttiii <ben.iburg@gmail.com>
2021-02-09 12:50:49 -0600

    delete unused themes

--- wordpress/wp-content/themes/wp/functions.php
+++ wordpress/wp-content/themes/wp/functions.php
@@ -28,13 +28,13 @@ if (! function_exists('setup')):
 endif;
 
 function scripts_header() {
-  wp_enqeue_style('init', get_stylesheet_uri()); //load css
+  wp_enqueue_style('init', get_stylesheet_uri()); //load css
 }
 
 function scripts_footer() {
-  // wp_enqeue_script('init', get_template_directory_uri().'/js/init.js', array('jquery')); //load js
+  // wp_enqueue_script('init', get_template_directory_uri().'/js/init.js', array('jquery')); //load js
 }
 
 add_action('after_setup_theme', 'setup'); //once theme is initialized, pulls functions
-add_action('wp_enqeue_scripts', 'scripts_header');
+add_action('wp_enqueue_scripts', 'scripts_header');
 // add_action('wp_footer', 'scripts_footer');
